2688db25813c9dfdda9a89adf93b226245513d7d
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-08-30 14:01:43 +0000

    Update Fonts

--- module/language.nix
+++ module/language.nix
@@ -3,38 +3,48 @@
 	i18n.defaultLocale = "zh_CN.UTF-8";
 
 	fonts = {
-		fontDir.enable = true;
 		fonts = with pkgs; [
-			noto-fonts
-			noto-fonts-cjk
-      noto-fonts-cjk-sans
-			noto-fonts-cjk-serif
+      noto-fonts
+      noto-fonts-cjk
       noto-fonts-emoji
 			sarasa-gothic  #更纱黑体
-			source-code-pro
-			hack-font
+      source-code-pro
+      source-han-mono
+      source-han-sans
+      source-han-serif
 			jetbrains-mono
 		];
+
+    fontDir.enable = true;
+    fontconfig.enable = true;
+
+    #enableDefaultFonts = true;
+    #fontconfig.defaultFonts = {
+    #  emoji = [ "Noto Color Emoji" ];
+    #  monospace = [ "Source Han Mono" ];
+    #  sansSerif = [ "Noto Sans CJK SC" ];
+    #  serif = [ "Source Han Serif" ];
+    #};
 	};
 
 	# 简单配置一下 fontconfig 字体顺序，以免 fallback 到不想要的字体
-	fonts.fontconfig = {
-		defaultFonts = {
-			emoji = ["Noto Color Emoji"];
-			monospace = [
-				"Noto Sans Mono CJK SC"
-          "DejaVu Sans Mono"
-        ];
-        sansSerif = [
-          "Noto Sans CJK SC"
-          "Source Han Sans SC"
-        ];
-        serif = [
-          "Noto Serif CJK SC"
-          "Source Han Serif SC"
-        ];
-      };
-    };
+	#fonts.fontconfig = {
+	#	defaultFonts = {
+	#		emoji = ["Noto Color Emoji"];
+	#		monospace = [
+	#			"Noto Sans Mono CJK SC"
+   #     "DejaVu Sans Mono"
+   #   ];
+   #   sansSerif = [
+   #     "Noto Sans CJK SC"
+   #     "Source Han Sans SC"
+   #   ];
+   #   serif = [
+   #     "Noto Serif CJK SC"
+   #     "Source Han Serif SC"
+   #   ];
+  #  };
+  #};
 
   i18n.inputMethod = {
     # enabled = "fcitx5";
@@ -43,8 +53,6 @@
     #   fcitx5-rime
     #   fcitx5-chinese-addons
     # ];
-
-    # 我现在用 ibus
     enabled = "ibus";
     ibus.engines = with pkgs.ibus-engines; [
       libpinyin
