5d532c396282b1986ae87ece00e87c2991e8f061
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-08-31 04:51:14 +0800

    Update gnome.nix

--- desktop/gnome.nix
+++ desktop/gnome.nix
@@ -8,14 +8,10 @@
 		gnome-photos
 		gnome-tour
 	]) ++ (with pkgs.gnome; [
-		cheese # webcam tool
-		gnome-music
-		gedit # text editor
 		epiphany # web browser
 		geary # email reader
 		evince # document viewer
 		gnome-characters
-		totem # video player
 		tali # poker game
 		iagno # go game
 		hitori # sudoku game
