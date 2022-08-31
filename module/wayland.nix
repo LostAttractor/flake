01f5fff5a9752f11b1d014780171074d30af38fb
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-08-31 20:54:02 +0800

    Update wayland.nix

--- module/wayland.nix
+++ module/wayland.nix
@@ -3,7 +3,7 @@
     programs.xwayland.enable = true;
     environment.sessionVariables = {
         # NIXOS_OZONE_WL = "1";
-        MOZ_ENABLE_WAYLAND = "1";
+        # MOZ_ENABLE_WAYLAND = "1";
         QT_QPA_PLATFORM="wayland;xcb";
     };
 }
