77191555e2bcbe76c45a77dd68b2d7eac420b8e1
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-09-01 03:39:20 +0800

    Update gnome-tweaks.nix

--- desktop/gnome-tweaks.nix
+++ desktop/gnome-tweaks.nix
@@ -1,23 +1,24 @@
 { config, pkgs, ... }:
 {
-	environment.systemPackages = with pkgs; [ 
-		gnome.gnome-tweaks
-		gnome.gnome-todo
-		gnome.dconf-editor
-		gnome.gnome-power-manager
-		gnome.gnome-sound-recorder
-
-		gnomeExtensions.appindicator
-		gnomeExtensions.gsconnect
-		gnomeExtensions.clipboard-history
-		gnomeExtensions.blur-my-shell
-		gnomeExtensions.dock-from-dash
-		gnomeExtensions.openweather
-		gnomeExtensions.hibernate-status-button
-
+	environment.systemPackages = (with pkgs; [ 
 		papirus-icon-theme
         orchis-theme
-	];
+	]) ++ (with pkgs.gnome; [
+		gnome-tweaks
+		gnome-todo
+		dconf-editor
+		gnome-power-manager
+		gnome-sound-recorder
+	]) ++ (with pkgs.gnomeExtensions; [
+		appindicator
+		gsconnect
+		clipboard-history
+		blur-my-shell
+		dock-from-dash
+		openweather
+		hibernate-status-button
+		ibus-tweaker
+	]);
 
 	services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
 	services.packagekit.enable = true;
@@ -25,7 +26,7 @@
 	programs.kdeconnect = {
 		enable = true;
 		package = pkgs.gnomeExtensions.gsconnect;
-	};
+	}; #开放网络端口
 
 	services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
 		[org.gnome.desktop.interface]
