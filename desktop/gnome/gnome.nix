659b23cd742401b686c01ed256d6413e6b7e410d
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-12-13 05:10:52 +0800

    Update gnome.nix

--- desktop/gnome/gnome.nix
+++ desktop/gnome/gnome.nix
@@ -5,19 +5,18 @@
 		desktopManager.gnome.enable = true;
 		displayManager.gdm.enable = true;
 	};
-	
+
 	environment.gnome.excludePackages = (with pkgs; [
 		gnome-photos
 		gnome-tour
 	]) ++ (with pkgs.gnome; [
 		epiphany # web browser
-		# geary # email reader
+		geary # email reader
 		evince # document viewer
 		gnome-characters
 		tali # poker game
 		iagno # go game
 		hitori # sudoku game
 		atomix # puzzle game
-		yelp
 	]);
 }
