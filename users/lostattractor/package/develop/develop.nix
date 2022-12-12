2677f42a02a9d31d0c6d45c247ffedc78d4c1ac5
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-12-13 04:12:09 +0800

    Update develop.nix

--- users/lostattractor/package/develop/develop.nix
+++ users/lostattractor/package/develop/develop.nix
@@ -1,18 +1,14 @@
 { config, pkgs, ... }:
 {
   home.packages = with pkgs; [
+    # Tools / IDE
     vscode
     lapce
-    github-desktop
-    # (github-desktop.override {
-    #   commandLineArgs = [
-    #     "--use-angle=vulkan"
-    #     "--use-cmd-decoder=passthrough"
-    #   ];
-    # })
+    # github-desktop
     jetbrains.idea-ultimate
     jetbrains.clion
     jetbrains.pycharm-professional
+    # Tools-Chain
     hugo
     # conda
     # (let 
