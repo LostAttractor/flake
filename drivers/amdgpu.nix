1ea43a6911d51cf2fa58b0543533dd0aeffdf1a1
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-12-13 23:53:03 +0800

    update driver

--- drivers/amdgpu.nix
+++ drivers/amdgpu.nix
@@ -3,12 +3,12 @@
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
 
-  hardware.opengl.driSupport = true;
-  
-  hardware.opengl.extraPackages = with pkgs; [
-    amdvlk
-    # rocm-opencl-icd
-  ];
-  # environment.variables.VK_ICD_FILENAMES =
-  #   "/run/opengl-driver/share/vulkan/icd.d/amd_icd64.json";
+  hardware.opengl = {
+    driSupport = true;
+    driSupport32Bit = true;
+    # extraPackages = with pkgs; [
+    #   # amdvlk
+    #   # rocm-opencl-icd
+    # ];
+  };
 }
--- drivers/nvidia.nix
+++ drivers/nvidia.nix
@@ -4,9 +4,17 @@
   nixpkgs.config.allowUnfree = true;
 
   services.xserver.videoDrivers = [ "nvidia" ];
+  
   hardware.opengl.enable = true;
 
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement = {
+      enable = true;
+      finegrained = true;
+    };
+  };
+
   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-  hardware.nvidia.modesetting.enable = true;
+  # hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 }
