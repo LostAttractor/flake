40acf730c338cfeb84dba908a647f277854f899b
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-12-13 17:00:36 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,43 +2,52 @@
   description = "ChaosAttractor's NixOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nur.url = "github:nix-community/NUR";
-    nur.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  # 输出配置，即 NixOS 系统配置
-  outputs = inputs@{ nixpkgs, home-manager, nur, ... }:
-  let system = "x86_64-linux";
-  in {
-    nixosConfigurations."CALaptop" = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ./configuration.nix
-        # Home-Manager
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.lostattractor = import ./users/lostattractor/home.nix;
-        }
-        # Enable NUR
-        nur.nixosModules.nur
-        # Config
-        ({ config, ... }: {
-          # Packages form NUR
-          home-manager.users.lostattractor = { ... }: {
-            home.packages = [
-              config.nur.repos.YisuiMilena.hyfetch
-              config.nur.repos.rewine.landrop
-              # config.nur.repos.xddxdd.wechat-uos-bin
-              config.nur.repos.linyinfeng.icalingua-plus-plus
-            ];
-          };
-        })
-      ];
+      # Nix Packages
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+      # User Packages
+      home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+      # NUR Packages
+      nur = {
+        url = "github:nix-community/NUR";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
     };
-  };
+
+  outputs = inputs @ { self, nixpkgs, home-manager, nur, ... }: # Function that tells my flake which to use and what do what to do with the dependencies.
+    let
+      user = "lostattractor";
+    in
+    {
+      nixosConfigurations."CALaptop" = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit nur; };
+        modules = [
+          ./configuration.nix          
+          # Enable NUR
+          nur.nixosModules.nur
+          # Home-Manager
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit user; };
+            home-manager.users.lostattractor = import ./users/lostattractor/home.nix;
+          }
+          # Config
+          ({ config, ... }: {
+            # Packages form NUR
+            home-manager.users.lostattractor = { ... }: {
+              home.packages = [
+                config.nur.repos.YisuiMilena.hyfetch
+                config.nur.repos.rewine.landrop
+                # config.nur.repos.xddxdd.wechat-uos-bin
+                config.nur.repos.linyinfeng.icalingua-plus-plus
+              ];
+            };
+          })
+        ];
+      };
+    };
 }
