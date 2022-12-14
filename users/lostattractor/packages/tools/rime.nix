0f54c3dadac284acd893157a704bc7993624db6f
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-12-15 02:45:59 +0800

    Update rime.nix

--- users/lostattractor/packages/tools/rime.nix
+++ users/lostattractor/packages/tools/rime.nix
@@ -4,7 +4,7 @@
   xdg.configFile."ibus/rime".source = pkgs.fetchFromGitHub ({
     owner = "LostAttractor";
     repo = "ca-rime-config";
-    rev = "6e9f039d4858db44b88ad48220e57b38d2ca1ae7";
-    sha256 = "00rhip8h3i988nby8nc7w4yp6ygfybv4v5hmwx4j888xmd4lwsbh";
+    rev = "b19df49b924a8d5e6ff5f35e987954382fd23755";
+    sha256 = "sha256-qqjTgwSEILuj6PRuK0wnI6ehZwNjML9fFFzFC4My++8=";
   });
 }
