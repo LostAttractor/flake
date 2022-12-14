4c4c09fc20f09e6c919f227e6b751ba7c8881d20
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-12-14 12:33:41 +0800

    Update default.nix

--- users/lostattractor/default.nix
+++ users/lostattractor/default.nix
@@ -1,9 +1,21 @@
 { config, pkgs, ... }:
 {
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.lostattractor = {
-        isNormalUser = true;
-        extraGroups = [ "wheel" "networkmanager" "docker" "libvirtd" ];  # Enable ‘sudo’ for the user.
-        shell = pkgs.fish;
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.lostattractor = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "docker" "libvirtd" ];  # Enable ‘sudo’ for the user.
+    shell = pkgs.fish;
+  };
+
+  fileSystems."/mnt/Windows" =
+    { device = "/dev/disk/by-uuid/40E6AD00E6ACF6F6";
+      fsType = "ntfs3"; 
+      options = [ "rw" "uid=1000" ];
+    };
+
+  fileSystems."/mnt/Files" =
+    { device = "/dev/disk/by-uuid/D246F12146F1074F";
+      fsType = "ntfs3"; 
+      options = [ "rw" "uid=1000" ];
     };
 }
