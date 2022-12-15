7bf6587ac8f0205e5ea8e2f50b0738d98bd45369
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-12-16 00:17:34 +0800

    Update fonts.nix

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -5,16 +5,17 @@
       noto-fonts
       noto-fonts-cjk
       noto-fonts-emoji
-	    sarasa-gothic  #更纱黑体
-      source-code-pro
+      # source-code-pro
       source-han-mono
       source-han-sans
       source-han-serif
 	    jetbrains-mono
-      wqy_zenhei
       hack-font
       fira-code
-      (nerdfonts.override { fonts = [ "JetBrainsMono" "Hack" ]; })
+      fira-code-symbols
+      wqy_zenhei
+      sarasa-gothic  #更纱黑体
+      (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "JetBrainsMono" "Hack" ]; })
 	  ];
     fontDir.enable = true;
     fontconfig.enable = true;
