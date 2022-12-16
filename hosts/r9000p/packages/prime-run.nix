eb6ae8850f34db609f7987f276ebd2cce205c33b
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-12-16 09:11:19 +0800

    remove hosts

--- hosts/r9000p/packages/prime-run.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-let
-  prime-run = pkgs.writeShellScriptBin "prime-run" ''
-    __NV_PRIME_RENDER_OFFLOAD=1 __VK_LAYER_NV_optimus=NVIDIA_only __GLX_VENDOR_LIBRARY_NAME=nvidia "$@"
-  '';
-in
-{
-  environment.systemPackages = [ prime-run ];
-}
