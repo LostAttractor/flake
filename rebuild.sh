242381b4ec1e4fb90937b7dab00d20f721d90623
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-12-19 14:42:19 +0800

    1

--- rebuild.sh
+++ rebuild.sh
@@ -3,7 +3,6 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-sudo rm -R /etc/nixos/*
 rsync -a --exclude={'update.sh','refresh.sh','rebuild.sh','.git'} /home/lostattractor/nixos/* /etc/nixos/ # -v
 cd /etc/nixos/
 nixos-rebuild switch -v
