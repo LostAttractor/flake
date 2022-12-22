f060da6a5741ccef9b209035ef05ad57307f191b
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2022-12-22 23:16:35 +0800

    adb

--- packages/develop.nix
+++ packages/develop.nix
@@ -3,4 +3,6 @@
   environment.systemPackages = with pkgs; [
     clang go deno docker nodejs yarn rustup go python3
   ];
+
+  programs.adb.enable = true;
 }
--- user/default.nix
+++ user/default.nix
@@ -3,7 +3,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${user} = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "docker" "libvirtd" ];  # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "networkmanager" "docker" "libvirtd" "adbusers" ];  # Enable ‘sudo’ for the user.
     shell = pkgs.fish;
   };
 
