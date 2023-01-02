77e52e087506a5fb5e86acdad1f207ec0a2ef920
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2023-01-02 23:43:26 +0800

    Update default.nix

--- user/packages/gaming/default.nix
+++ user/packages/gaming/default.nix
@@ -1,14 +1,19 @@
 { config, pkgs, ... }:
-{
-  programs.mangohud.enable = true;
-  programs.mangohud.enableSessionWide = true;
-  
+{ 
   home.packages = with pkgs; [
     osu-lazer-bin
     # lunar-client
     protonup-ng
   ];
-  
+
+  programs.mangohud.enable = true;
+  programs.mangohud.enableSessionWide = true;
+
+  systemd.user.sessionVariables = {
+    MANGOHUD = 1;
+    MANGOHUD_DLSYM = 1;
+  };
+
   # MANGOHUD=1 gamemoderun DXVK_ASYNC=1 %command% -novid -high +fps_max 144
   # gamemoderun DXVK_ASYNC=1 %command%
 }
