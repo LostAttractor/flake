8bfcde4c2b330f46ea4bd56a132206f28a6b12a6
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2023-01-02 15:08:54 +0800

    xdg

--- desktop/gnome/default.nix
+++ desktop/gnome/default.nix
@@ -3,6 +3,7 @@
   imports = [ 
 	  ./gnome.nix
     ./wayland.nix
+    ./xdg.nix
   ];
 
   services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
---
+++ desktop/gnome/xdg.nix
@@ -0,0 +1,8 @@
+{ config, ... }:
+
+{
+  xdg.portal = { 
+    enable = true;
+    xdgOpenUsePortal = true; 
+  };
+}
