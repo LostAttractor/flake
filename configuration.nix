334ae8d2cc3edb39e00c0659ce0d829d03e7aaf5
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2023-01-10 13:55:14 +0800

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,7 @@
       # ./modules/power-management.nix
       # package
       ./packages
+      ./tests
       # user config
       ./user
     ];
@@ -77,10 +78,10 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 15234 ]; #landrop
+  networking.firewall.allowedUDPPorts = [ 15234 ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
