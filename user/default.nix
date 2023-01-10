7f341265ca322d2cab90ff83a75fdbde9a5e1d1b
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2023-01-10 13:55:17 +0800

    Update default.nix

--- user/default.nix
+++ user/default.nix
@@ -7,15 +7,17 @@
     shell = pkgs.fish;
   };
 
-  # fileSystems."/mnt/Windows" =
-  #   { device = "/dev/disk/by-uuid/40E6AD00E6ACF6F6";
-  #     fsType = "ntfs3"; 
-  #     options = [ "rw" "uid=1000" ];
-  #   };
+  fileSystems."/mnt/Windows" =
+    { device = "/dev/disk/by-uuid/40E6AD00E6ACF6F6";
+      fsType = "ntfs3"; 
+      options = [ "rw" "uid=1000" ];
+      noCheck = true;
+    };
 
-  # fileSystems."/mnt/Files" =
-  #   { device = "/dev/disk/by-uuid/D246F12146F1074F";
-  #     fsType = "ntfs3"; 
-  #     options = [ "rw" "uid=1000" ];
-  #   };
+  fileSystems."/mnt/Files" =
+    { device = "/dev/disk/by-uuid/D246F12146F1074F";
+      fsType = "ntfs3"; 
+      options = [ "rw" "uid=1000" ];
+      noCheck = true;
+    };
 }
