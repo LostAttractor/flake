df1b178630fb163ccd5c2c88d430dd837c7aeb68
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2023-01-12 01:31:01 +0800

    flatpak filesystems

--- user/modules/flatpak/default.nix
+++ user/modules/flatpak/default.nix
@@ -2,6 +2,6 @@
 {
   home.file.".local/share/flatpak/overrides/global".text = ''
     [Context]
-    filesystems=/run/current-system/sw/share/X11/fonts:ro;/nix/store:ro;~/.local/share/fonts:ro
+    filesystems=/nix/store:ro;/run/current-system/sw/share/X11/fonts:ro;/run/current-system/sw/share/icons:ro;~/.local/share/fonts:ro;~/.local/share/icons:ro;/usr/share/fonts:ro;/usr/share/icons:ro
   '';
 }
