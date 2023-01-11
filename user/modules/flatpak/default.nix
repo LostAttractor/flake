11f3fb0f4a169662d1e32316e2bbf5ecddf46c53
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2023-01-12 01:34:38 +0800

    only usr/share/fonts;icons

--- user/modules/flatpak/default.nix
+++ user/modules/flatpak/default.nix
@@ -2,6 +2,6 @@
 {
   home.file.".local/share/flatpak/overrides/global".text = ''
     [Context]
-    filesystems=/nix/store:ro;/run/current-system/sw/share/X11/fonts:ro;/run/current-system/sw/share/icons:ro;~/.local/share/fonts:ro;~/.local/share/icons:ro;/usr/share/fonts:ro;/usr/share/icons:ro
+    filesystems=/usr/share/fonts:ro;/usr/share/icons:ro
   '';
 }
