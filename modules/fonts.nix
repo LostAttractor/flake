8aa27346fd62bc1caefd3fea0d057f6cb1e14384
ChaosAttractor <lostattractor@gmail.com>
2023-01-16 10:09:48 +0800

    Update fonts.nix

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -2,19 +2,30 @@
 {
   fonts = {
 	  fonts = with pkgs; [
+      # noto
       noto-fonts
-      noto-fonts-cjk
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
       noto-fonts-emoji
-      # source-code-pro
-      source-han-mono
+      # source
+      source-code-pro
+      source-sans
+      source-sans-pro
+      source-serif
+      source-serif-pro
+      source-han-code-jp
       source-han-sans
       source-han-serif
+      source-han-mono
+      # jetbrains
 	    jetbrains-mono
-      hack-font
+      # fira
       fira-code
       fira-code-symbols
+      # wqy
       wqy_zenhei
-      sarasa-gothic  #更纱黑体
+      #更纱黑体
+      sarasa-gothic
       (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "JetBrainsMono" "Hack" ]; })
 	  ];
     fontDir.enable = true;
