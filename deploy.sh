db455eedc9df7d3979e31200154d4024b9e77204
ChaosAttractor <lostattractor@gmail.com>
2023-01-17 03:09:06 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-sudo rm -R /etc/nixos/*
+rm -R /etc/nixos/*
 rsync -a --exclude={'deploy.sh','flake.sh','refresh.sh','.git'} ./* /etc/nixos/ # -v
 nixos-rebuild switch -v
