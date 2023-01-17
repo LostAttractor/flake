6a83fcad5b8b60a78e978455f37ebd311c818ae7
ChaosAttractor <lostattractor@gmail.com>
2023-01-17 16:54:38 +0800

    yesplaymusic

--- user/packages/basic.nix
+++ user/packages/basic.nix
@@ -15,6 +15,7 @@
     signal-desktop
     (discord.override { nss = nss_latest; })
     (keeweb.override { nss = nss_latest; })
+    yesplaymusic
     # (makeAutostartItem { name = "org.telegram.desktop"; package = tdesktop; })
     # (makeAutostartItem { name = "qq"; package = qq; })
     (pkgs.callPackage ../../userrepo/makeAutostartItem { name = "org.telegram.desktop"; package = tdesktop; commandLineArgs = "-startintray"; })
