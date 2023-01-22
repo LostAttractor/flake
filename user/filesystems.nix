121a1d20f527b9fb8a4e1e1aa8a509bc44eb0524
ChaosAttractor <lostattractor@gmail.com>
2023-01-22 14:03:39 +0800

    ntfs3

--- user/filesystems.nix
+++ user/filesystems.nix
@@ -2,14 +2,14 @@ _:
 {
   fileSystems."/mnt/Windows" =
    { device = "/dev/disk/by-uuid/88103C73103C69F8";
-     fsType = "ntfs"; #ntfs(ntfs-3g) ntfs3
+     fsType = "ntfs3"; #ntfs(ntfs-3g) ntfs3
      options = [ "rw" "uid=1000" ];
      noCheck = true;
    };
 
   fileSystems."/mnt/Files" =
    { device = "/dev/disk/by-uuid/D246F12146F1074F";
-     fsType = "ntfs"; 
+     fsType = "ntfs3"; 
      options = [ "rw" "uid=1000" ];
      noCheck = true;
    };
