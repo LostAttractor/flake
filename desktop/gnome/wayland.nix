d6d2b15fa6b6f09b1b28b73c86c241ebc4995deb
ChaosAttractor <lostattractor@gmail.com>
2023-01-22 14:04:40 +0800

    Update wayland.nix

--- desktop/gnome/wayland.nix
+++ desktop/gnome/wayland.nix
@@ -1,8 +1,8 @@
 _:
 {
   # programs.xwayland.enable = true; //Defalut
-  environment.sessionVariables = {
-    QT_QPA_PLATFORM = "wayland;xcb";
-    # NIXOS_OZONE_WL = "1";
-  };
+  # environment.sessionVariables = {
+  #   QT_QPA_PLATFORM = "wayland;xcb";
+  #   NIXOS_OZONE_WL = "1";
+  # };
 }
