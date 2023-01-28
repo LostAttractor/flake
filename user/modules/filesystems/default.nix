da2cd1d1de7e6af207be224a407bd158d5ad763b
ChaosAttractor <46527539+LostAttractor@users.noreply.github.com>
2023-01-28 22:52:53 +0800

    Update Filesystems

--- user/modules/filesystems/default.nix
+++ user/modules/filesystems/default.nix
@@ -1,9 +1,8 @@
-_:
+{ pkgs, user, ... }:
 {
-  fileSystems."/mnt/Files" =
-   { device = "/dev/disk/by-uuid/D246F12146F1074F";
-     fsType = "ntfs"; 
-     options = [ "rw" "uid=1000" ];
-     noCheck = true;
-   };
+  fileSystems."/run/media/${user}/Files" =
+    { device = "/dev/disk/by-uuid/f177fc67-c5a4-4ab9-abd1-6d9ec94ed446";
+      fsType = "btrfs";
+      options = [ "compress=zstd" ];
+    };
 }
