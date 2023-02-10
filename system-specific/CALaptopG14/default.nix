00f8ccaa0e8c70f0f043008152b22f9898a0a6fc
ChaosAttractor <lostattractor@gmail.com>
2023-02-10 15:32:41 +0800

    fix vamillo keybroad fnmode

--- system-specific/CALaptopG14/default.nix
+++ system-specific/CALaptopG14/default.nix
@@ -5,7 +5,7 @@
   programs.rog-control-center.enable = true;
   programs.rog-control-center.autoStart = true;
 
-  services.fprintd.enable = true;
-  services.fprintd.tod.enable = true;
-  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
+  boot.extraModprobeConfig = ''
+    options hid_apple fnmode=0
+  '';
 }
--- system-specific/CALaptopR9000P/default.nix
+++ system-specific/CALaptopR9000P/default.nix
@@ -3,7 +3,11 @@
   networking.hostName = "CALaptopR9000P"; # Define hostname.
 
   imports = [
-    ./modules/features/rathole
+    # ./modules/features/rathole
     ./modules/features/gpu-paththrough
   ];
+
+  boot.extraModprobeConfig = ''
+    options hid_apple fnmode=0
+  '';
 }
