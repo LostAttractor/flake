9f92b4d0865df1066e042e6d575ed5c131df5c1b
ChaosAttractor <lostattractor@gmail.com>
2023-02-19 12:33:39 +0800

    asusd

--- system-specific/CALaptopG14/default.nix
+++ system-specific/CALaptopG14/default.nix
@@ -5,6 +5,8 @@
   programs.rog-control-center.enable = true;
   programs.rog-control-center.autoStart = true;
 
+  services.asusd.enable = true;
+
   boot.extraModprobeConfig = ''
     options hid_apple fnmode=0
   '';
