16b697c44cde1b44909a93b923fc51a5f06d2c34
ChaosAttractor <lostattractor@gmail.com>
2023-03-10 16:07:56 +0800

    vscode ext

--- user/packages/develop/vscode/default.nix
+++ user/packages/develop/vscode/default.nix
@@ -8,67 +8,6 @@
       ms-vscode.cpptools
       ms-vscode-remote.remote-ssh
       rust-lang.rust-analyzer
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "gitlens";
-        publisher = "eamodio";
-        version = "13.2.0";
-        sha256 = "sha256-/4GQuQqeFpBmreARVIVkqXP462y14gT0ZgV39e1FK40=";
-      }
-      {
-        name = "vscode-pull-request-github";
-        publisher = "GitHub";
-        version = "0.58.2";
-        sha256 = "sha256-T/ZlhvepqLf4pDUTM2TIfDxoF2XjSdFLDJFgyEy4M1g=";
-      }
-      {
-        name = "vscode-docker";
-        publisher = "ms-azuretools";
-        version = "1.23.3";
-        sha256 = "05lxa03hh5rslg7351dw87zlrp4drqy1jmpms1aml0yn1jxfb9yj";
-      }
-      {
-        name = "vscode-language-pack-zh-hans";
-        publisher = "MS-CEINTL";
-        version = "1.75.2023021509";
-        sha256 = "074ibsxhgmrri2z351fzx3wv2sr5kdqmd9i2gi2n6sl7xy866mv9";
-      }
-      {
-        name = "cmake-tools";
-        publisher = "ms-vscode";
-        version = "1.14.13";
-        sha256 = "0caml20npjdg4cs08gm6nyqs3w5vipa3syag3sx03iyhfry7b4wk";
-      }
-      {
-        name = "cpptools-extension-pack";
-        publisher = "ms-vscode";
-        version = "1.3.0";
-        sha256 = "11fk26siccnfxhbb92z6r20mfbl9b3hhp5zsvpn2jmh24vn96x5c";
-      }
-      {
-        name = "cpptools-themes";
-        publisher = "ms-vscode";
-        version = "2.0.0";
-        sha256 = "05r7hfphhlns2i7zdplzrad2224vdkgzb0dbxg40nwiyq193jq31";
-      }
-      {
-        name = "material-icon-theme";
-        publisher = "PKief";
-        version = "4.24.0";
-        sha256 = "0wqglzrby0idz4m3jzwsrlhnbgipg491wzzkpsvdc5z5d75bx744";
-      }
-      {
-        name = "cmake";
-        publisher = "twxs";
-        version = "0.0.17";
-        sha256 = "11hzjd0gxkq37689rrr2aszxng5l9fwpgs9nnglq3zhfa1msyn08";
-      }
-      {
-        name = "material-theme";
-        publisher = "zhuangtongfa";
-        version = "3.15.7";
-        sha256 = "1hxh5wg39h5k3p2cgh8vdh7l0pxsrw2cvfwbhsx8ifs8748qagi5";
-      }
-    ];
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace (import ./extensions.nix).extensions;
   };
 }
---
+++ user/packages/develop/vscode/extensions.nix
@@ -0,0 +1,69 @@
+{ extensions = [
+  {
+    name = "gitlens";
+    publisher = "eamodio";
+    version = "2023.3.904";
+    sha256 = "1da4a7x30llyl1456vvabgckv06w0xcbaqsx8kxpzi3p0i4256h8";
+  }
+  {
+    name = "vscode-pull-request-github";
+    publisher = "GitHub";
+    version = "0.61.2023030909";
+    sha256 = "093x6p16bc4ysa6yfaajm1803qrnp095dviwz0ybvahvwl7j46qg";
+  }
+  {
+    name = "vscode-docker";
+    publisher = "ms-azuretools";
+    version = "1.24.0";
+    sha256 = "1fgkqcjxg77n434jjna8hs7qrj1cmq060sb7gidsagni18lzi7fd";
+  }
+  {
+    name = "vscode-language-pack-zh-hans";
+    publisher = "MS-CEINTL";
+    version = "1.76.2023030809";
+    sha256 = "0f2bg5nm4sybwf84afvhc22yjp66rzdz4s1iaa31yxb4c1ij2vsr";
+  }
+  {
+    name = "cmake-tools";
+    publisher = "ms-vscode";
+    version = "1.14.15";
+    sha256 = "1kils3rjkkl7xdc8wyw0df094syr0g3w8k5yzs4i0rs0n7bqd3hn";
+  }
+  {
+    name = "cpptools-extension-pack";
+    publisher = "ms-vscode";
+    version = "1.3.0";
+    sha256 = "11fk26siccnfxhbb92z6r20mfbl9b3hhp5zsvpn2jmh24vn96x5c";
+  }
+  {
+    name = "cpptools-themes";
+    publisher = "ms-vscode";
+    version = "2.0.0";
+    sha256 = "05r7hfphhlns2i7zdplzrad2224vdkgzb0dbxg40nwiyq193jq31";
+  }
+  {
+    name = "hexeditor";
+    publisher = "ms-vscode";
+    version = "1.9.11";
+    sha256 = "0annfim67kmc0fxd8ap00z5sv7hwwp5q2rlxm0n1099znk7pqm63";
+  }
+  {
+    name = "material-icon-theme";
+    publisher = "PKief";
+    version = "4.24.0";
+    sha256 = "0wqglzrby0idz4m3jzwsrlhnbgipg491wzzkpsvdc5z5d75bx744";
+  }
+  {
+    name = "cmake";
+    publisher = "twxs";
+    version = "0.0.17";
+    sha256 = "11hzjd0gxkq37689rrr2aszxng5l9fwpgs9nnglq3zhfa1msyn08";
+  }
+  {
+    name = "material-theme";
+    publisher = "zhuangtongfa";
+    version = "3.15.8";
+    sha256 = "1s7dlklzlyma3yv5bnahla8yg2qm7hn7dq46lpm5szcisarqc19z";
+  }
+];
+}
