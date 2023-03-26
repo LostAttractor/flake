4b3dfb5ff4ebc1b5688a836bec8c6974bd8f9cea
ChaosAttractor <lostattractor@gmail.com>
2023-03-27 00:39:52 +0800

    Update ca-rime-config

--- user/packages/tools/rime/default.nix
+++ user/packages/tools/rime/default.nix
@@ -4,7 +4,7 @@
   xdg.configFile."ibus/rime".source = pkgs.fetchFromGitHub {
     owner = "LostAttractor";
     repo = "ca-rime-config";
-    rev = "6d95e48bcfdb289597fdf91156b8e3b68c841c5c";
-    sha256 = "sha256-1H6cSe7jgvXQxfdPqwZCki0DAbNIcwfY2C1y6VXVl7U=";
+    rev = "62fdb53d89618b17b48dce28bd2f05b3f9fac8d1";
+    sha256 = "sha256-pMcUPw1tHp3fYDH3Mb93Va2uCovzoQzmxQU8Q+5fqV0=";
   };
 }
