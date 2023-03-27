528cd53c08078635c2b3adbef4b6399b26998778
ChaosAttractor <lostattractor@gmail.com>
2023-03-27 15:29:07 +0800

    BUG: 如果不设置 LANG=zh_CN.utf8 可能导致ibus中文输入异常

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -2,6 +2,8 @@
 {
 	# i18n.defaultLocale = "zh_CN.UTF-8";
 
+  # BUG: 如果不设置 LANG=zh_CN.utf8 可能导致ibus中文输入异常
+
   i18n.inputMethod = {
     enabled = "ibus";
     ibus.engines = with pkgs.ibus-engines; [ rime ];
