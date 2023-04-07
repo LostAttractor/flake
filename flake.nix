aa1f0d88f3ef7eeb9bb61aaa685c1e96f94611f2
ChaosAttractor <lostattractor@gmail.com>
2023-04-08 01:22:03 +0800

    Update flake.nix
    Use staging-next-testing for testing gnome 44

--- flake.nix
+++ flake.nix
@@ -4,8 +4,9 @@
   inputs = {
     # Nix Packages
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
-    nixpkgs.url = "github:lostattractor/nixpkgs/nixos-unstable-small-custom";
-    # nixpkgs.url = "github:lostattractor/nixpkgs/master-custom";
+    # nixpkgs.url = "github:lostattractor/nixpkgs/nixos-unstable-small-testing";
+    # nixpkgs.url = "github:lostattractor/nixpkgs/master-testing";
+    nixpkgs.url = "github:lostattractor/nixpkgs/staging-next-testing";
     # Nix Hardware
     nixos-hardware.url = "github:nixos/nixos-hardware";
     # lanzaboote (Secure boot)
