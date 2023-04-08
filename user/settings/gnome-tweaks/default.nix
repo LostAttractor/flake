50336750e4df7ec399d931e39cb2db86a843feb7
ChaosAttractor <lostattractor@gmail.com>
2023-04-08 18:44:21 +0800

    Re: dconf & add sleep-inactive-ac-type = "nothing";

--- user/settings/gnome-tweaks/default.nix
+++ user/settings/gnome-tweaks/default.nix
@@ -1,14 +1,15 @@
 _:
 {
-  dconf = {
-    enable = true;
-    settings = {
-      "org.gnome.desktop.interface" = {
-        gtk-theme = "adw-gtk3-dark";
-      };
-      "org/gnome/desktop/peripherals/touchpad" = {
-        tap-to-click = true;
-      };
+  dconf.enable = true;
+  dconf.settings = {
+    "org.gnome.desktop.interface" = {
+      gtk-theme = "adw-gtk3-dark";
+    };
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true;
+    };
+    "org/gnome/settings-daemon/plugins/power" = {
+      sleep-inactive-ac-type = "nothing";
     };
   };
 }
