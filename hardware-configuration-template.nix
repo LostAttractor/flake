265bf3385719eec5b69ac315aa9d1adb0d3f1013
ChaosAttractor <lostattractor@gmail.com>
2023-04-12 18:30:05 +0800

    Update hardware-configuration-template.nix

--- hardware-configuration-template.nix
+++ hardware-configuration-template.nix
@@ -8,53 +8,53 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+  # https://nixos.org/manual/nixos/stable/options.html#opt-boot.resumeDevice
+  boot.resumeDevice = "/dev/disk/by-uuid/fe8d27b3-11bf-4939-a414-d1d359c26083";
   # https://wiki.archlinux.org/title/Power_management/Suspend_and_hibernate#Hibernation_into_swap_file_on_Btrfs
-  boot.kernelParams = [ "resume=/swap/swapfile" "resume_offset=82067968" ];
+  # https://sawyershepherd.org/post/hibernating-to-an-encrypted-swapfile-on-btrfs-with-nixos/
+  boot.kernelParams = [ "resume_offset=5514496" ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/f177fc67-c5a4-4ab9-abd1-6d9ec94ed446";
+    { device = "/dev/disk/by-uuid/fe8d27b3-11bf-4939-a414-d1d359c26083";
       fsType = "btrfs";
       options = [ "subvol=root" "compress=zstd" ];
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/f177fc67-c5a4-4ab9-abd1-6d9ec94ed446";
+    { device = "/dev/disk/by-uuid/fe8d27b3-11bf-4939-a414-d1d359c26083";
       fsType = "btrfs";
       options = [ "subvol=home" "compress=zstd" ];
     };
 
   fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/f177fc67-c5a4-4ab9-abd1-6d9ec94ed446";
+    { device = "/dev/disk/by-uuid/fe8d27b3-11bf-4939-a414-d1d359c26083";
       fsType = "btrfs";
       options = [ "subvol=nix" "compress=zstd" "noatime" ];
     };
     
   fileSystems."/swap" =
-    { device = "/dev/disk/by-uuid/f177fc67-c5a4-4ab9-abd1-6d9ec94ed446";
+    { device = "/dev/disk/by-uuid/fe8d27b3-11bf-4939-a414-d1d359c26083";
       fsType = "btrfs";
       options = [ "subvol=swap" "noatime" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/74CD-2753";
+    { device = "/dev/disk/by-uuid/C784-FC44";
       fsType = "vfat";
     };
 
   swapDevices = [ { device = "/swap/swapfile"; } ];
-  # https://nixos.org/manual/nixos/stable/options.html#opt-boot.resumeDevice
-  boot.resumeDevice = "/dev/disk/by-uuid/f177fc67-c5a4-4ab9-abd1-6d9ec94ed446";
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
