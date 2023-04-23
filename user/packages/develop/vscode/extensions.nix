bffb8da5ec627b6169357cac79c2d03298b22eae
ChaosAttractor <lostattractor@gmail.com>
2023-04-24 01:52:44 +0800

    Update extensions.nix

--- user/packages/develop/vscode/extensions.nix
+++ user/packages/develop/vscode/extensions.nix
@@ -2,14 +2,14 @@
   {
     name = "gitlens";
     publisher = "eamodio";
-    version = "2023.4.1805";
-    sha256 = "1r5jwib3qbvk0w7wlm8api1s4fyy8hkv7wl8mbjr69gszs1ky43w";
+    version = "2023.4.2005";
+    sha256 = "01fbygwygj91c48rwk1rlyv2nwszp3q1xk11cbi506xy1y03any7";
   }
   {
     name = "vscode-pull-request-github";
     publisher = "GitHub";
-    version = "0.63.2023041809";
-    sha256 = "1jl70v9i614yilq4anh7l5fjjkbhm1vbhaw602a1g8bqib92i2c9";
+    version = "0.63.2023042009";
+    sha256 = "034fka9c306d66kh1kpda1is7i4jvrbl2hw4mqya6y2dilb9q4v8";
   }
   {
     name = "vscode-docker";
@@ -20,14 +20,14 @@
   {
     name = "vscode-language-pack-zh-hans";
     publisher = "MS-CEINTL";
-    version = "1.77.2023040509";
-    sha256 = "1fln06p1kqdiwraldifv9d7z993m43vmpssrybnsd8xqwdgw1k5z";
+    version = "1.78.2023041909";
+    sha256 = "09fd06vml4ryz7wdgs5hcal2jkhqn4ax42925305a42s3fr1p9h8";
   }
   {
     name = "cmake-tools";
     publisher = "ms-vscode";
-    version = "1.14.27";
-    sha256 = "17varvzfbrx7p7v5l2l6aj9qv4v3ipx2r0r9wq0ncamgmdm29wgb";
+    version = "1.15.0";
+    sha256 = "1g961ba0qhq03xj6i4gwg826khvxva6qjrp877vjf7ig6ykqq4jk";
   }
   {
     name = "cpptools-extension-pack";
