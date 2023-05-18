0bc1bcbe167f9722d3ae5987dd9ca799114a2f8f
ChaosAttractor <lostattractor@gmail.com>
2023-05-19 02:46:42 +0800

    hardware-specific: apple-silicon

--- specific/hardware-specific/apple-silicon/default.nix
+++ specific/hardware-specific/apple-silicon/default.nix
@@ -3,8 +3,10 @@
   hardware.asahi.useExperimentalGPUDriver = true;
   # hardware.asahi.experimentalGPUInstallMode = "overlay";
 
-  hardware.asahi.peripheralFirmwareDirectory = ./firmware;
+  # hardware.asahi.peripheralFirmwareDirectory = ./firmware;
   #hardware.asahi.extractPeripheralFirmware = false;
+
+  boot.loader.timeout = lib.mkForce 5;
 
   boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
 }
