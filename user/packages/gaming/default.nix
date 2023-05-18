cb5f1eb15085ae8437df34504905eb6e7eb98b79
ChaosAttractor <lostattractor@gmail.com>
2023-05-19 02:36:50 +0800

    yuzu & vulkan icd comment

--- user/packages/gaming/default.nix
+++ user/packages/gaming/default.nix
@@ -5,6 +5,7 @@
     lunar-client
     protonup-ng
     lutris
+    yuzu
     gwe
   ];
 
@@ -18,4 +19,10 @@
 
   # MANGOHUD=1 gamemoderun DXVK_ASYNC=1 %command% -novid -high +fps_max 144
   # gamemoderun DXVK_ASYNC=1 %command%
+
+  # Force vulkan loader: 
+  # environment.variables.VK_ICD_FILENAMES =
+  # "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
+  # export VK_ICD_FILENAMES="/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json"
+  # https://nixos.org/manual/nixos/unstable/index.html#sec-gpu-accel-vulkan
 }
