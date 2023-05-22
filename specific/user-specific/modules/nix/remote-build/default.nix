4aa45bbc26e242865af4ba819473acc0ff08831a
ChaosAttractor <lostattractor@gmail.com>
2023-05-23 01:10:40 +0800

    remote-build: maxJobs 10 to 16

--- specific/user-specific/modules/nix/remote-build/default.nix
+++ specific/user-specific/modules/nix/remote-build/default.nix
@@ -19,7 +19,7 @@
       # if the builder supports building for multiple architectures, 
       # replace the previous line by, e.g.,
       # systems = ["x86_64-linux" "aarch64-linux"];
-      maxJobs = 10;
+      maxJobs = 16;
       speedFactor = 1;
       supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
       mandatoryFeatures = [ ];
