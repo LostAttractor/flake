d651887dfcffee6d6942a8d30b8f374606b36194
ChaosAttractor <lostattractor@gmail.com>
2023-06-02 01:02:13 +0800

    Testing

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     # Nix Packages
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
-    # nixpkgs.url = "github:lostattractor/nixpkgs/nixos-unstable-small-testing";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:lostattractor/nixpkgs/nixos-unstable-small-testing";
     # nixpkgs.url = "github:nixos/nixpkgs/master";
     # Nix Hardware
     nixos-hardware.url = "github:nixos/nixos-hardware";
