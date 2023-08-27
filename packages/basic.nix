da13dd11fa7b4433c3ef1e9d1614fcbf6bb37a53
ChaosAttractor <lostattractor@gmail.com>
2023-08-27 09:30:10 +0800

    Packages: Add ipmitool

--- packages/basic.nix
+++ packages/basic.nix
@@ -6,7 +6,7 @@
     openssl wget axel # Downloader
     git vim nano # Develop
     htop micro xsel # Basic Tools
-    pciutils usbutils bridge-utils dnsutils ethtool iperf3 mtr # Utils / Tools
+    pciutils usbutils bridge-utils dnsutils ethtool iperf3 mtr ipmitool # Utils / Tools
     v2ray # Proxy
     appimage-run # Run Appimage 
     rsync # Rsync
