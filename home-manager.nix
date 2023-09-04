5e66c2525e52f8546495871633efe4df3467348d
ChaosAttractor <lostattractor@gmail.com>
2023-09-05 04:25:54 +0800

    Rename nur-modules

--- home-manager.nix
+++ home-manager.nix
@@ -1,12 +1,12 @@
-{ inputs, user, nur, nixpkgs, ... }: 
+{ inputs, user, ... }: 
 let
-  nur-modules = import inputs.nur {
+  nur = import inputs.nur {
     nurpkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
     pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
   };
 in {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.extraSpecialArgs = { inherit inputs user nur-modules; };
+  home-manager.extraSpecialArgs = { inherit inputs user nur; };
   home-manager.users.${user} = import ./user/home.nix;
 }
