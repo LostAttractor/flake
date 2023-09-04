7a8003b382ae07d77cb44c9a72a8d157ebbe5a58
ChaosAttractor <lostattractor@gmail.com>
2023-09-05 04:05:18 +0800

    KVMFR

--- user/modules/looking-glass/default.nix
+++ user/modules/looking-glass/default.nix
@@ -1,7 +1,21 @@
 { user, ... }:
+let
+  looking-glass-client-config = ''
+    [win]
+    fullScreen=yes
+
+    [input]
+    escapeKey=97
+
+    [spice]
+    captureOnStart=yes
+  '';
+in
 {
   systemd.tmpfiles.rules = [ 
     # Type Path               Mode UID     GID Age Argument
     "f /dev/shm/looking-glass 0660 ${user} kvm -"
   ];
+
+  environment.etc."looking-glass-client.ini".text = looking-glass-client-config;
 }
---
+++ user/modules/looking-glass/kvmfr/default.nix
@@ -0,0 +1,41 @@
+{ config, user, ... }:
+let
+  looking-glass-client-config = ''
+    [app]
+    shmFile=/dev/kvmfr0
+
+    [win]
+    fullScreen=yes
+
+    [input]
+    escapeKey=97
+
+    [spice]
+    captureOnStart=yes
+  '';
+in
+{
+  boot = {
+    kernelModules = ["kvmfr"];
+    extraModulePackages = with config.boot.kernelPackages; [ kvmfr ];
+    extraModprobeConfig = ''
+      options kvmfr static_size_mb=64
+    '';
+  };
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="kvmfr", OWNER="${user}", GROUP="kvm", MODE="0660"
+  '';
+
+  virtualisation.libvirtd.qemu.verbatimConfig = ''
+    namespaces = []
+    cgroup_device_acl = [
+      "/dev/null", "/dev/full", "/dev/zero",
+      "/dev/random", "/dev/urandom",
+      "/dev/ptmx", "/dev/kvm",
+      "/dev/kvmfr0"
+    ]
+  '';
+
+  environment.etc."looking-glass-client.ini".text = looking-glass-client-config;
+}
