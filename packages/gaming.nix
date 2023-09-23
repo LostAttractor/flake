fbbdb4545408313cb78f4ee55fc662d27c307076
ChaosAttractor <lostattractor@gmail.com>
2023-09-24 06:11:47 +0800

    AAGL

--- packages/gaming.nix
+++ packages/gaming.nix
@@ -1,10 +1,16 @@
-_:
+{ inputs, ...}:
 {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
+
+  nix.settings = inputs.aagl.nixConfig; # Set up Cachix
+  programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
+  programs.anime-borb-launcher.enable = true;
+  programs.honkers-railway-launcher.enable = true;
+  programs.honkers-launcher.enable = true;
 
   programs.gamemode.enable = true;
 }
