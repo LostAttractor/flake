1b3cb14283d2bdae4f4f836f54fbd42341287385
ChaosAttractor <lostattractor@gmail.com>
2023-10-27 05:13:59 +0800

    desktop/gnome: Reorganize & Add gnome-remote-desktop

--- desktop/gnome/default.nix
+++ desktop/gnome/default.nix
@@ -1,8 +1,27 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  imports = [ 
-    ./gnome.nix
-    ./wayland.nix
-    ./xdg.nix
-  ];
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    epiphany # web browser
+  ]);
+
+  # Enable Wayland for QT and Chromium Apps
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    QT_QPA_PLATFORM = "wayland";
+  };
+
+  # Fix that some apps cannot open links
+  xdg.portal = { 
+    enable = true;
+    xdgOpenUsePortal = true; 
+  };
+
+  # Enable Gnome Remote Display
+  services.gnome.gnome-remote-desktop.enable = true;
 }
