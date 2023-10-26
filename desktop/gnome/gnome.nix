b8943328813791eed870ffbe32df7d8b9bb656d0
ChaosAttractor <lostattractor@gmail.com>
2023-10-27 02:47:07 +0800

    desktio/gnome: Reorganize and remove  unused

--- desktop/gnome/gnome.nix
+++ desktop/gnome/gnome.nix
@@ -1,25 +1,12 @@
 { pkgs, ... }:
 {
-  services = {
-    xserver = {
-      desktopManager.gnome.enable = true;
-      displayManager.gdm.enable = true;
-    };
-	  packagekit.enable = true;
-    udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
-  };
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   environment.gnome.excludePackages = (with pkgs; [
-    gnome-photos
     gnome-tour
   ]) ++ (with pkgs.gnome; [
     epiphany # web browser
-    geary # email reader
-    evince # document viewer
-    gnome-characters
-    tali # poker game
-    iagno # go game
-    hitori # sudoku game
-    atomix # puzzle game
   ]);
 }
