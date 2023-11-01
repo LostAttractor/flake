74efedb1ce79c8e4583bf20c8876facea3759a49
ChaosAttractor <lostattractor@gmail.com>
2023-11-02 05:09:56 +0800

    Add self-hosted hydra

--- modules/nix/substituters.nix
+++ modules/nix/substituters.nix
@@ -1,4 +1,7 @@
 _:
 {
-  nix.settings.substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
+  nix.settings = {
+    substituters = [ "https://binarycache.home.lostattractor.net" "https://mirror.sjtu.edu.cn/nix-channels/store" ];
+    trusted-public-keys = [ "binarycache.home.lostattractor.net:nB258qoytYrdCe2pcI6qJ/M9R0l7Q5l9Bu5ryCbzItc=" ];
+  };
 }
