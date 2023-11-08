0124e6f88e2ec997f204ddda58b377a04b53f2fa
ChaosAttractor <lostattractor@gmail.com>
2023-11-08 11:34:50 +0800

    Change iBus to Fcitx5

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -5,7 +5,10 @@
   # BUG: 如果不设置 LANG=zh_CN.utf8 可能导致ibus中文输入异常
 
   i18n.inputMethod = {
-    enabled = "ibus";
-    ibus.engines = with pkgs.ibus-engines; [ rime ];
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-rime
+      fcitx5-chinese-addons
+    ];
   };
 }
