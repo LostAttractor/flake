9a5096570125d88c55611c64fdf847a0c60b9222
ChaosAttractor <lostattractor@gmail.com>
2023-11-21 01:29:24 +0800

    Remove  NoXpad that is already unused

--- specific/hardware-specific/asus-zephyrus-ga401/modules/features/gpu-paththrough/specialisation.nix
+++ specific/hardware-specific/asus-zephyrus-ga401/modules/features/gpu-paththrough/specialisation.nix
@@ -14,7 +14,6 @@ in
   specialisation."GPUPaththrough".configuration = {
     system.nixos.tags = [ 
       "Nvidia-GPU-VFIO"
-      # "NoXpad"
       ];
 
     # The vfio modules before the nvidia modules is very intentional because it lets vfio claim my GPU before nvidia does.
@@ -34,15 +33,5 @@ in
     boot.kernelParams = [ 
       ("vfio-pci.ids=" + lib.concatStringsSep "," gpuIDs) # vfio devices
     ];
-
-    # Xpad affects the work of the xbox controller and its wireless adapter
-    # The xpad will shake hands with the handle/wireless adapter when it is plugged in. At this time, 
-    # if you pass the usb device directly to the virtual machine, the xbox handle will not re-handshake with the root of windows,
-    # which will eventually cause it to fail to work.
-    # I can't find a way to make the usb device passthrough into the virtual machine from before/when it is plugged in,
-    # so I suggest you disable this driver if you need to use the gamepad in virtual machine
-
-    # Now using libvirt hooks to unload xpad dynamicly
-    # boot.blacklistedKernelModules = [ "xpad" ];
   };
 }
