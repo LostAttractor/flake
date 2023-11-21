9597381b224a8cb0751be9f492294e08b3014667
ChaosAttractor <lostattractor@gmail.com>
2023-11-22 01:48:38 +0800

    ga401/features/nvidia: Init

--- specific/hardware-specific/asus-zephyrus-ga401/default.nix
+++ specific/hardware-specific/asus-zephyrus-ga401/default.nix
@@ -3,7 +3,7 @@
   imports = [
     ./modules/features/gpu-paththrough
     ./modules/features/virtualisation
-    ./modules/features/nvidia-open
+    ./modules/features/nvidia
     # ./modules/features/libfprint-goodix-521d
   ];
 }
