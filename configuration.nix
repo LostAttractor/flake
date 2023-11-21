8aab2686df0c68485af3bfb3a70c394f95c26e15
ChaosAttractor <lostattractor@gmail.com>
2023-11-22 05:49:08 +0800

    configuartion.nix: Reorganize & Optimize

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,8 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports =
@@ -40,8 +40,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.consoleMode = lib.mkDefault "auto";
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.timeout = 180; # 3min
-  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_zen; #Linux-zen
+  boot.loader.timeout = 180;
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
   # boot.tmp.cleanOnBoot = false;
 
   nixpkgs.config.permittedInsecurePackages = [
@@ -70,22 +70,27 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
   # system.copySystemConfiguration = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.05"; # Did you read the comment?
 
 }
