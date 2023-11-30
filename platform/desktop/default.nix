ddf7633e9fe518e7edba2e546835e1684f852e33
ChaosAttractor <lostattractor@gmail.com>
2023-11-30 14:14:57 +0800

    Add Gnome & Hyprland specialisation

---
+++ platform/desktop/default.nix
@@ -0,0 +1,23 @@
+_:
+{
+  imports = [
+    # Using Gnome by default
+    ./gnome
+    # platform/modules/basic
+    ../modules/fonts.nix
+    # platform/modules/hardware
+    ../modules/hardware/bluetooth.nix
+    ../modules/hardware/gamepad.nix
+    ../modules/hardware/mobiledevice.nix
+    # platform/modules/features
+    ../modules/features/pipewire.nix
+    ../modules/features/printing.nix
+    ../modules/features/flatpak.nix
+  ];
+
+  specialisation."Hyprland".configuration = {
+    system.nixos.tags = [ "Hyprland" ];
+    imports = [ ./hyprland ];
+    disabledModules = [ ./gnome ];
+  };
+}
