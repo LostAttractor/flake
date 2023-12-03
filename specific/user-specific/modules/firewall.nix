4dc6b840770439b9758b1b568df32ff0373ae66c
ChaosAttractor <lostattractor@gmail.com>
2023-12-04 04:30:36 +0800

    Moving uxplay module to firewall settings & avahi module

--- specific/user-specific/modules/firewall.nix
+++ specific/user-specific/modules/firewall.nix
@@ -3,11 +3,16 @@
   # Firewall Ports
   # 3389  RDP        TCP
   # 15234 Landrop    TCP
+  # 7000  Uxplay     TCP
+  # 7100  Uxplay     TCP
+  # 6000  Uxplay     UDP
+  # 6001  Uxplay     UDP
+  # 7011  Uxplay     UDP
   # 51820 Wireguard  UDP (Configured via NetworkManager)
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 15234 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 15234 7000 7100 ];
+  networking.firewall.allowedUDPPorts = [ 6000 6001 7011 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
