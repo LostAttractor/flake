c3c7513adb3afab81887cbf27c11f0c5a273c88e
ChaosAttractor <lostattractor@gmail.com>
2023-12-03 22:19:22 +0800

    Only allow unfree packages  in home-manager or for flakes that needed proprietary driver

--- configuration.nix
+++ configuration.nix
@@ -40,15 +40,10 @@
   boot.tmp.useTmpfs = true;
   # You can choose whether to clean /tmp on boot, but this is not necessary for Tmpfs
   # boot.tmp.cleanOnBoot = true;
-
-  # https://github.com/NixOS/nixpkgs/issues/258048
-  nixpkgs.config.permittedInsecurePackages = [ "electron-22.3.27" ];
   
   nix.settings.experimental-features = [ "nix-command" "flakes" "ca-derivations" ];
 
   nix.settings.trusted-users = [ "root" "@wheel" ];
-
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
