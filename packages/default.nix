09db197f62f773982868cc0fbcb559ba6be24fdf
ChaosAttractor <lostattractor@gmail.com>
2023-12-05 01:44:18 +0800

    packages: optimize
    Reorganize
    Remove unused tools
    Remove desktop apps like firefox
    Remove unused comment in configuration.nix
    
    1

--- packages/default.nix
+++ packages/default.nix
@@ -1,8 +1,28 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  imports = [ 
-	  ./basic.nix
-    ./develop.nix
-    ./gaming.nix
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget axel # Download Tools
+    git vim micro nano # Editor & VCS
+    htop tmux # Basic Tools
+    pciutils usbutils bridge-utils # Utils
+    dnsutils ethtool iperf3 # Network Tools
+    ipmitool # IPMI
+    v2ray xray sing-box # Proxy
+    appimage-run # Run Appimage 
+    rsync # Rsync
+    clang go rustup gcc # Compiler
+    nodejs yarn # NodeJS
+    python3 # Python
   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+
+  programs.sharing.enable = true;
+
+  # Android debug bridge
+  programs.adb.enable = true;
 }
