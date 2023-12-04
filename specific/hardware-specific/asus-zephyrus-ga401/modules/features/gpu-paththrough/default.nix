e1097e563299eeefbd188c3d553ac4fa5433ee1b
ChaosAttractor <lostattractor@gmail.com>
2023-12-05 01:39:30 +0800

    Remove pcie-acs-override for GA401

--- specific/hardware-specific/asus-zephyrus-ga401/modules/features/gpu-paththrough/default.nix
+++ specific/hardware-specific/asus-zephyrus-ga401/modules/features/gpu-paththrough/default.nix
@@ -4,11 +4,4 @@
     ./specialisation.nix
     ./hooks.nix
   ];
-
-  boot.kernelParams = [ 
-    "pcie_acs_override=downstream,multifunction" # PCI-E ACS (For paththrough iGPUs)
-    # "video=efifb:off"
-    # "vfio-pci.disable_vga=1" 
-    # "vfio-pci.disable_idle_d3=1" 
-  ];
 }
