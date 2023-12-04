8e6a71789df0403ba5966f9bd85d3ef592945132
ChaosAttractor <lostattractor@gmail.com>
2023-12-05 02:17:17 +0800

    platform/desktop: separate modules into a new file

---
+++ platform/desktop/modules.nix
@@ -0,0 +1,16 @@
+_:
+{
+  imports = [
+    # platform/modules/basic
+    ../modules/fonts.nix
+    # platform/modules/hardware
+    ../modules/hardware/bluetooth.nix
+    ../modules/hardware/gamepad.nix
+    ../modules/hardware/mobiledevice.nix
+    # platform/modules/features
+    ../modules/features/pipewire.nix
+    ../modules/features/avahi.nix
+    ../modules/features/printing.nix
+    ../modules/features/flatpak.nix
+  ];
+}
