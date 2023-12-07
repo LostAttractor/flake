38f960f87a9185d008965d45bc3c967f2993d295
ChaosAttractor <lostattractor@gmail.com>
2023-12-07 14:52:15 +0800

    user/desktop/gnome/dconf: enable resize-with-right-button

--- user/desktop/gnome/dconf.nix
+++ user/desktop/gnome/dconf.nix
@@ -7,6 +7,7 @@ _:
     };
     "org/gnome/desktop/wm/preferences" = {
       button-layout = "appmenu:minimize,maximize,close";
+      resize-with-right-button = true;
     };
     "org/gnome/settings-daemon/plugins/power" = {
       sleep-inactive-ac-type = "nothing";
