2778115cf8f32eea5b07b2943e791f2f1245d47c
ChaosAttractor <lostattractor@gmail.com>
2023-12-08 21:10:59 +0800

    tpm/pkcs11: init

--- user/default.nix
+++ user/default.nix
@@ -3,7 +3,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${user} = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "docker" "libvirtd" "adbusers" ];  # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "tss" "networkmanager" "docker" "libvirtd" "adbusers" ];  # Enable ‘sudo’ for the user.
     shell = pkgs.fish;
     initialHashedPassword = "$6$ER3vW7b0o74VUb5S$MhA3IXzTJpJMkfLXZiPjVHQdmHRQX9Axfi2G5nLJIS/Hm98rw7.Pd39JeVDgPrvWUUJZAH8BfSdHLMyJI2zLu/";
   };
