f037279afea65adf83bb8c2da342222ea2e738a8
ChaosAttractor <lostattractor@gmail.com>
2023-12-08 16:32:51 +0800

    user-specific/gnupg: init

--- specific/user-specific/default.nix
+++ specific/user-specific/default.nix
@@ -5,6 +5,7 @@
     ./modules/firewall.nix
     ./modules/nix/access-tokens.nix
     ./modules/nix/remote-builds.nix
+    ./modules/features/gnupg.nix
     ./modules/features/waydroid.nix
     ./modules/features/looking-glass/kvmfr.nix
   ];
---
+++ specific/user-specific/modules/features/gnupg.nix
@@ -0,0 +1,7 @@
+_:
+{
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+}
