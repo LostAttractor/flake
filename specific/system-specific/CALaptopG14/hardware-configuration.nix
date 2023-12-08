b5727993c0c295bfad07f09088caf5fdab02cc8a
ChaosAttractor <lostattractor@gmail.com>
2023-12-08 21:10:59 +0800

    tpm/pkcs11: init

--- specific/system-specific/CALaptopG14/hardware-configuration.nix
+++ specific/system-specific/CALaptopG14/hardware-configuration.nix
@@ -21,7 +21,14 @@
   boot.initrd.systemd.enable = true;
 
   security.tpm2.enable = true;
+  security.tpm2.pkcs11.enable = true;
+  security.tpm2.pkcs11.package = pkgs.tpm2-pkcs11.override { abrmdSupport = false; }; # Using Kernel RM
   security.tpm2.tctiEnvironment.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    (tpm2-tools.override { abrmdSupport = false; })
+    tpm2-tss
+  ];
 
   boot.initrd.luks.devices."lvm-encrypted".device = "/dev/disk/by-uuid/468b2786-745a-4937-9123-4f4e5ba58d12";
 
