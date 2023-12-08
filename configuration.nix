5b4b7029c1c3d19dd698b7a25383af1f3a965d49
ChaosAttractor <lostattractor@gmail.com>
2023-12-09 00:33:31 +0800

    stateVersion: sync to 24.05

--- configuration.nix
+++ configuration.nix
@@ -70,7 +70,7 @@
   # and migrated your data accordingly.
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "24.05"; # Did you read the comment?
 
 }
 
