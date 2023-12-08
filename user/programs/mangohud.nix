6f569a46160c4ec8d17dacd72bb0eb75dd9daa32
ChaosAttractor <lostattractor@gmail.com>
2023-12-08 23:16:55 +0800

    user: reclassification & update code style

--- user/programs/mangohud.nix
+++ user/programs/mangohud.nix
@@ -1,13 +1,5 @@
-{ pkgs, ... }:
-{ 
-  home.packages = with pkgs; [
-    osu-lazer-bin
-    lunar-client
-    protonup-ng
-    lutris
-    yuzu-early-access
-  ];
-
+_:
+{
   programs.mangohud.enable = true;
 
   # MANGOHUD=1 gamemoderun DXVK_ASYNC=1 %command% -novid -high +fps_max 144
