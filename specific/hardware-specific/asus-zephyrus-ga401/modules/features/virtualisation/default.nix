b50fc05a659975aa6a91d13a328e7a0c67f2169c
ChaosAttractor <lostattractor@gmail.com>
2023-12-12 17:43:55 +0800

    iommu: amd_iommu is enabled by default in kernel config

--- specific/hardware-specific/asus-zephyrus-ga401/modules/features/virtualisation/default.nix
+++ specific/hardware-specific/asus-zephyrus-ga401/modules/features/virtualisation/default.nix
@@ -1,6 +1,7 @@
 # You need kvm-intel / kvm-amd kernelModule for virtualisation
 _:
 {
-  boot.kernelParams = [ "amd_iommu=pt" ];  # Enable IOMMU
+  # AMD IOMMU is enabled by default
+  # https://wiki.archlinux.org/title/PCI_passthrough_via_OVMF#Enabling_IOMMU
   boot.extraModprobeConfig = "options kvm_amd nested=1"; # nested virtualization
 }
