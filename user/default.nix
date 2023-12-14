9f424ca79d68f787a27ed00de76180a7b1b06bc2
ChaosAttractor <lostattractor@gmail.com>
2023-12-15 02:46:15 +0800

    user: fish is already the default shell

--- user/default.nix
+++ user/default.nix
@@ -1,10 +1,9 @@
-{ pkgs, user, ... }:
+{ user, ... }:
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${user} = {
     isNormalUser = true;
     extraGroups = [ "wheel" "tss" "networkmanager" "docker" "libvirtd" "adbusers" ];  # Enable ‘sudo’ for the user.
-    shell = pkgs.fish;
     initialHashedPassword = "$6$ER3vW7b0o74VUb5S$MhA3IXzTJpJMkfLXZiPjVHQdmHRQX9Axfi2G5nLJIS/Hm98rw7.Pd39JeVDgPrvWUUJZAH8BfSdHLMyJI2zLu/";
   };
 }
