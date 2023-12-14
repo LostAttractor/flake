75ece64a1a488b9b8679991a301c0f3810785135
ChaosAttractor <lostattractor@gmail.com>
2023-12-15 03:13:24 +0800

    platform/desktop/features/inputmethod: init

--- user/settings/i18n.nix
+++ user/settings/i18n.nix
@@ -1,19 +1,4 @@
-{ pkgs, config, inputs, ... }:
+_:
 {
 	# i18n.defaultLocale = "zh_CN.UTF-8";
-
-  # BUG: 如果不设置 LANG=zh_CN.utf8 可能导致ibus中文输入异常
-
-  i18n.inputMethod = {
-    enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-rime
-      fcitx5-chinese-addons
-    ];
-  };
-
-  # xdg.dataFile."fcitx5/rime" = {
-  #   recursive = true;
-  #   source = inputs.rime-ice;
-  # };
 }
