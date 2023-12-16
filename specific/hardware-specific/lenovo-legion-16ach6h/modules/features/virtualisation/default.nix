dfbd45f8952e9116d038d24524a62f5f3529f5f2
ChaosAttractor <lostattractor@gmail.com>
2023-12-17 02:08:33 +0800

    specific: share duplicate functionality

--- specific/hardware-specific/lenovo-legion-16ach6h/modules/features/virtualisation/default.nix
+++
@@ -1,7 +0,0 @@
-# You need kvm-intel / kvm-amd kernelModule for virtualisation
-_:
-{
-  # AMD IOMMU is enabled by default
-  # https://wiki.archlinux.org/title/PCI_passthrough_via_OVMF#Enabling_IOMMU
-  boot.extraModprobeConfig = "options kvm_amd nested=1"; # nested virtualization
-}
