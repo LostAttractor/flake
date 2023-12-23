c53d051688ccb7efaaefd763778a2375b3587cd9
ChaosAttractor <lostattractor@gmail.com>
2023-12-24 01:59:15 +0800

    user/desktop/hyprland: optimize

--- user/desktop/hyprland/config.nix
+++ user/desktop/hyprland/config.nix
@@ -9,7 +9,7 @@
     settings = {
       # See https://wiki.hyprland.org/Configuring/Monitors/
       monitor = [
-        "eDP-1,preferred,auto,1.3"
+        "eDP-1,preferred,auto,1.25"
         "HDMI-A-1,highrr,auto,auto"
       ];
 
@@ -37,11 +37,12 @@
       input = {
         touchpad = { natural_scroll = true; };
         sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+        follow_mouse = 2;
       };
 
       general = {
         gaps_in = 5;
-        gaps_out = 20;
+        gaps_out = 8;
         border_size = 2;
         "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
         "col.inactive_border" = "rgba(595959aa)";
@@ -53,7 +54,7 @@
       };
 
       decoration = {
-        rounding = 10;
+        rounding = 8;
 
         blur = {
           enabled = true;
@@ -80,6 +81,7 @@
           "borderangle, 1, 8, default"
           "fade, 1, 7, default"
           "workspaces, 1, 6, default"
+          "specialWorkspace, 1, 6, default, slidevert"
         ];
       };
 
@@ -122,14 +124,24 @@
       # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
       bind = [
         "$mainMod, R, exec, pkill rofi || rofi -show drun -show-icons"
-        "$mainMod, Q, exec, kitty"
+        "$mainMod, RETURN, exec, kitty"
         "$mainMod, E, exec, nautilus"
-        "$mainMod, C, killactive,"
+        "$mainMod, W, killactive,"
         "$mainMod, A, fullscreen,"
         "$mainMod, M, exit,"
         "$mainMod, V, togglefloating,"
+        "$mainMod, P, pin,"
         "$mainMod, P, pseudo," # dwindle
         "$mainMod, J, togglesplit," # dwindle
+        "$mainMod, G, togglegroup,"
+
+        "$mainMod, Tab, changegroupactive, f"
+        "$mainMod SHIFT, Tab, changegroupactive, b"
+
+        "$mainMod, Tab, cyclenext,"
+        "$mainMod, Tab, bringactivetotop,"
+        "$mainMod SHIFT, Tab, cyclenext, prev"
+        "$mainMod SHIFT, Tab, bringactivetotop,"
 
         # Move focus with mainMod + arrow keys
         "$mainMod, left, movefocus, l"
