dda240d22775965cb0438d009aa61907f9093597
ChaosAttractor <lostattractor@gmail.com>
2024-01-30 05:24:44 +0800

    packages/gaming: enable gamescopeSession for steam

--- packages/gaming.nix
+++ packages/gaming.nix
@@ -2,6 +2,7 @@
 {
   programs.steam = {
     enable = true;
+    gamescopeSession.enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
