0bd9b34f2df0730f8964b491f5bc5246cd35955e
ChaosAttractor <lostattractor@gmail.com>
2024-02-20 12:16:47 +0800

    user: add description

--- user/default.nix
+++ user/default.nix
@@ -1,9 +1,9 @@
 { user, ... }:
 {
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${user} = {
+    description = "ChaosAttractor";
     isNormalUser = true;
     extraGroups = [ "wheel" "tss" "networkmanager" "docker" "libvirtd" "adbusers" ];  # Enable ‘sudo’ for the user.
-    initialHashedPassword = "$6$ER3vW7b0o74VUb5S$MhA3IXzTJpJMkfLXZiPjVHQdmHRQX9Axfi2G5nLJIS/Hm98rw7.Pd39JeVDgPrvWUUJZAH8BfSdHLMyJI2zLu/";
+    initialHashedPassword = "$6$ER3vW7b0o74VUb5S$MhA3IXzTJpJMkfLXZiPjVHQdmHRQX9Axfi2G5nLJIS/Hm98rw7.Pd39JeVDgPrvWUUJZAH8BfSdHLMyJI2zLu/"; # Set passwd directly
   };
 }
