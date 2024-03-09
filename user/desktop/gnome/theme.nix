b4fa708c071de0179dde8ebb0f9b7e3e80350071
ChaosAttractor <lostattractor@gmail.com>
2024-03-10 05:44:25 +0800

    user/settings/gtk: init from user/desktop/gnome/theme

--- user/desktop/gnome/theme.nix
+++ user/desktop/gnome/theme.nix
@@ -6,18 +6,6 @@
     gtk.enable = true;
   };
 
-  gtk = {
-    enable = true;
-    theme = {
-      name = "adw-gtk3-dark";
-      package = pkgs.adw-gtk3;
-    };
-    iconTheme = {
-      name = "Papirus";
-      package = pkgs.papirus-icon-theme;
-    };
-  };
-
   qt = {
     enable = true;
     platformTheme = "gnome";
