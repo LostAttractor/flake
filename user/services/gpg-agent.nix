cfb07c6b39f7620a8b118e2181a64497aefa4143
ChaosAttractor <lostattractor@gmail.com>
2024-03-12 14:38:25 +0800

    user/services/gpg-agent: init

---
+++ user/services/gpg-agent.nix
@@ -0,0 +1,15 @@
+_:
+{
+  # https://wiki.archlinux.org/title/GnuPG
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+  };
+
+  # Disable gnome-keyring-ssh
+  xdg.configFile."autostart/gnome-keyring-ssh.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Hidden=true
+  '';
+}
