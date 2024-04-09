e6d5514f47205b01a5a7f1b66a054076ac33243d
ChaosAttractor <lostattractor@gmail.com>
2024-04-09 20:11:50 +0800

    user/programs/obs-studio: add some plugins

--- user/programs/obs-studio.nix
+++ user/programs/obs-studio.nix
@@ -4,6 +4,10 @@
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
       obs-pipewire-audio-capture
+      obs-ndi
+      obs-text-pthread
+      obs-vkcapture
+      waveform
       # Plugins that no longer in use
       # obs-multi-rtmp
     ];
