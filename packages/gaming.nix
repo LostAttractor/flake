a064e1825125eb0af504a91cead8673b3304b017
ChaosAttractor <lostattractor@gmail.com>
2024-04-09 21:07:22 +0800

    use programs.steam.extraCompatPackages instead protonup-ng

--- packages/gaming.nix
+++ packages/gaming.nix
@@ -1,10 +1,11 @@
-{ inputs, ...}:
+{ inputs, pkgs, ...}:
 {
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
   };
 
   programs.gamescope.enable = true;
