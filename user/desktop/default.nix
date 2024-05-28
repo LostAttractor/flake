48e7b661a73463e83681178f40fa3d416d88f9a4
ChaosAttractor <lostattractor@gmail.com>
2024-05-29 00:03:05 +0800

    user/desktop: enable vulkan for gsk_renderer(gtk4 apps)

--- user/desktop/default.nix
+++ user/desktop/default.nix
@@ -2,4 +2,6 @@ _:
 {
   # Enable Wayland for Chromium (CEF) Apps
   home.sessionVariables.NIXOS_OZONE_WL = "1";
+  # Using vulkan renderer for gtk4
+  home.sessionVariables.GSK_RENDERER = "vulkan";
 }
