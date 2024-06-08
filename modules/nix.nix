b0a609445dc66167bfc6af6dab7c1616cf39c690
ChaosAttractor <lostattractor@gmail.com>
2024-06-09 05:19:38 +0800

    modules/nix: simplify to single file

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,7 +1,18 @@
 _:
 {
+  # Substituters
   nix.settings = {
     substituters = [ "https://binarycache.home.lostattractor.net" "https://mirror.sjtu.edu.cn/nix-channels/store" ];
     trusted-public-keys = [ "binarycache.home.lostattractor.net:nB258qoytYrdCe2pcI6qJ/M9R0l7Q5l9Bu5ryCbzItc=" ];
+  };
+
+  # Optimize nix store by using hard-link
+  nix.settings.auto-optimise-store = true; 
+
+  # Automatic GC
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 7d";
   };
 }
