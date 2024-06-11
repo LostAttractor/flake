b915eb2c65f513289a8daf0d03402e6516549f02
ChaosAttractor <lostattractor@gmail.com>
2024-06-11 23:13:41 +0800

    modules/nix: change substituters to ustc mirror

--- modules/nix.nix
+++ modules/nix.nix
@@ -2,7 +2,7 @@ _:
 {
   # Substituters
   nix.settings = {
-    substituters = [ "https://binarycache.home.lostattractor.net" "https://mirror.sjtu.edu.cn/nix-channels/store" ];
+    substituters = [ "https://binarycache.home.lostattractor.net" "https://mirrors.ustc.edu.cn/nix-channels/store" ];
     trusted-public-keys = [ "binarycache.home.lostattractor.net:nB258qoytYrdCe2pcI6qJ/M9R0l7Q5l9Bu5ryCbzItc=" ];
   };
 
