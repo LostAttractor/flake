bacd058489392850809013b6456522d3bad20288
ChaosAttractor <lostattractor@gmail.com>
2024-06-11 23:19:46 +0800

    user-specific/modules: moving firewall to network/firewall

--- specific/user-specific/default.nix
+++ specific/user-specific/default.nix
@@ -2,9 +2,9 @@
 {
   imports = [
     ./modules/sudo.nix
-    ./modules/firewall.nix
     ./modules/nix/access-tokens.nix
     ./modules/nix/remote-builds.nix
+    ./modules/network/firewall.nix
     ./modules/features/gnupg.nix
     ./modules/features/waydroid.nix
     ./modules/features/looking-glass/kvmfr.nix
