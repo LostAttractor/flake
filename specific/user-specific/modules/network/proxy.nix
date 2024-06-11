f19fd5674cb237b2e21dd772cd67b04af2c04222
ChaosAttractor <lostattractor@gmail.com>
2024-06-11 23:20:33 +0800

    user-specific/modules/proxy: init from modules/network

---
+++ specific/user-specific/modules/network/proxy.nix
@@ -0,0 +1,9 @@
+_:
+{
+  # Network Proxy
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # V2rayA
+  services.v2raya.enable = true;
+}
