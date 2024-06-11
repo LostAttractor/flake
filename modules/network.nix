638f6aa41f6f608f9c15de78a10efc37f119ff7c
ChaosAttractor <lostattractor@gmail.com>
2024-06-11 23:20:33 +0800

    user-specific/modules/proxy: init from modules/network

--- modules/network.nix
+++ modules/network.nix
@@ -2,11 +2,4 @@ _:
 {
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Network Proxy
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # V2rayA
-  services.v2raya.enable = true;
 }
--- specific/user-specific/default.nix
+++ specific/user-specific/default.nix
@@ -5,6 +5,7 @@
     ./modules/nix/access-tokens.nix
     ./modules/nix/remote-builds.nix
     ./modules/network/firewall.nix
+    ./modules/network/proxy.nix
     ./modules/features/gnupg.nix
     ./modules/features/waydroid.nix
     ./modules/features/looking-glass/kvmfr.nix
