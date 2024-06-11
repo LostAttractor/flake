e9f595642553155ef8ab731d0621f08db2bbde6f
ChaosAttractor <lostattractor@gmail.com>
2024-06-11 23:16:38 +0800

    user/programs/spicetify: optimize

--- user/programs/spicetify.nix
+++ user/programs/spicetify.nix
@@ -1,26 +1,34 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, config, ... }:
 let
   spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
+  spotify-adblock = pkgs.callPackage ../../userrepo/spotify-adblock { };
 in
 {
   # import the flake's module for your system
   imports = [ inputs.spicetify-nix.homeManagerModule ];
 
   # configure spicetify :)
-  programs.spicetify =
-    {
-      # Spotify Adblocked
-      spotifyPackage = pkgs.callPackage ../../userrepo/spotify { spotify-adblock = pkgs.callPackage ../../userrepo/spotify-adblock { }; };
-      windowManagerPatch = false;
+  programs.spicetify = {
+    enable = true;
+    dontInstall = true;
+    windowManagerPatch = true;
 
-      enable = true;
-      theme = spicePkgs.themes.Sleek;
-      colorScheme = "cherry";
+    theme = spicePkgs.themes.Sleek;
+    colorScheme = "cherry";
 
-      enabledExtensions = with spicePkgs.extensions; [
-        fullAppDisplay
-        shuffle # shuffle+ (special characters are sanitized out of ext names)
-        hidePodcasts
-      ];
-    };
+    enabledExtensions = with spicePkgs.extensions; [
+      fullAppDisplay
+      shuffle # shuffle+ (special characters are sanitized out of ext names)
+      hidePodcasts
+    ];
+  };
+
+  home.packages = [
+    (config.programs.spicetify.spicedSpotify.overrideAttrs (oldAttrs: {
+      postInstall = oldAttrs.postInstall or "" + ''
+        wrapProgram $out/bin/${oldAttrs.meta.mainProgram} \
+          --set LD_PRELOAD "${spotify-adblock}/lib/libspotifyadblock.so"
+      '';
+    }))
+  ];
 }
