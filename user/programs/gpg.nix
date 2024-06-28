91337784677993a04be85f63bec59c9e76cc2e3c
ChaosAttractor <lostattractor@gmail.com>
2024-06-29 04:51:58 +0800

    user/programs/gpg: init from user-specific

--- user/programs/gpg.nix
+++ user/programs/gpg.nix
@@ -1,8 +1,12 @@
-_: {
+{ osConfig, ... }:
+{
+  programs.gpg.enable = true;
+
   # https://wiki.archlinux.org/title/GnuPG
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
+    pinentryPackage = osConfig.programs.gnupg.agent.pinentryPackage;
   };
 
   # Disable gnome-keyring-ssh
