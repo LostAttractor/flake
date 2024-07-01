abbd94ba9fe9b4d853561714105da18e139e9b28
ChaosAttractor <lostattractor@gmail.com>
2024-07-02 01:25:17 +0800

    scripts/deploy-flake.sh: rsync is not necessary

--- scripts/deploy-flake.sh
+++ scripts/deploy-flake.sh
@@ -3,5 +3,4 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-rsync -rt --delete --exclude={'scripts','.git'} ./* /etc/nixos/ # -v
-nixos-rebuild switch $* --log-format internal-json -v |& nom --json
+nixos-rebuild switch --flake .# $* --log-format internal-json -v |& nom --json
