143296f6253710ba3390e82b745cee91d8d67b8f
ChaosAttractor <lostattractor@gmail.com>
2024-07-06 23:51:20 +0800

    specific/general/btrfs/snapper: new format & new limit

--- specific/general/btrfs/snapper.nix
+++ specific/general/btrfs/snapper.nix
@@ -5,9 +5,11 @@ _: {
       ALLOW_GROUPS = [ "wheel" ];
       TIMELINE_CREATE = true;
       TIMELINE_CLEANUP = true;
-      TIMELINE_LIMIT_WEEKLY = "3";
-      TIMELINE_LIMIT_MONTHLY = "0";
-      TIMELINE_LIMIT_YEARLY = "0";
+      TIMELINE_LIMIT_HOURLY = 24;
+      TIMELINE_LIMIT_DAILY = 7;
+      TIMELINE_LIMIT_WEEKLY = 3;
+      TIMELINE_LIMIT_MONTHLY = 0;
+      TIMELINE_LIMIT_YEARLY = 0;
     };
   };
 }
