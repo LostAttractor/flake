9be5df4566a832f2df99edb7fca2becf8b7b5208
ChaosAttractor <lostattractor@gmail.com>
2024-07-20 07:13:50 +0800

    hardware-specific/asus-zephyrus-ga401/features/libfprint-goodix-521d: optimize by using overlay

--- specific/hardware-specific/asus-zephyrus-ga401/modules/features/libfprint-goodix-521d/default.nix
+++ specific/hardware-specific/asus-zephyrus-ga401/modules/features/libfprint-goodix-521d/default.nix
@@ -1,20 +1,24 @@
-{ pkgs, ... }:
-with pkgs;
-{
+_: {
+  # This module is broken now
   services.fprintd.enable = true;
-  services.fprintd.package = fprintd.override {
-    libfprint = pkgs.libfprint.overrideAttrs (oldAttrs: {
-      pname = "libfprint-goodix-521d";
-      version = "unstable";
 
-      src = pkgs.fetchFromGitHub {
-        owner = "lostattractor";
-        repo = "libfprint";
-        rev = "abf8dffabfcc7373d4562c151d41a6c7e087ee41";
-        sha256 = "sha256-TEU/c3IfNL53vvEbA/ZSfq6ZKuo1g3alBIF1957ZVec=";
-      };
+  nixpkgs.overlays = [
+    (final: prev: {
+      libfprint = prev.libfprint.overrideAttrs (
+        oldAttrs: with prev; {
+          pname = "libfprint-goodix-521d";
+          version = "unstable";
 
-      buildInputs = oldAttrs.buildInputs or [ ] ++ [ openssl ];
-    });
-  };
+          src = fetchFromGitHub {
+            owner = "lostattractor";
+            repo = "libfprint";
+            rev = "abf8dffabfcc7373d4562c151d41a6c7e087ee41";
+            sha256 = "sha256-TEU/c3IfNL53vvEbA/ZSfq6ZKuo1g3alBIF1957ZVec=";
+          };
+
+          buildInputs = oldAttrs.buildInputs or [ ] ++ [ openssl ];
+        }
+      );
+    })
+  ];
 }
