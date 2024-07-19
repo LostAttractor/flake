d938c2c2d2b1f56f643c9692ca915621ef596629
ChaosAttractor <lostattractor@gmail.com>
2024-07-20 07:16:49 +0800

    platform/ime: migrate to new format

--- platform/modules/features/fcitx5.nix
+++ platform/modules/features/fcitx5.nix
@@ -1,10 +1,8 @@
 { pkgs, ... }:
 {
   i18n.inputMethod = {
-    enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-rime
-      fcitx5-chinese-addons
-    ];
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons ];
   };
 }
--- platform/modules/features/ibus.nix
+++ platform/modules/features/ibus.nix
@@ -2,7 +2,8 @@
 {
   # BUG: 如果不设置 LANG=zh_CN.utf8 可能导致ibus中文输入异常
   i18n.inputMethod = {
-    enabled = "ibus";
+    enable = true;
+    type = "ibus";
     ibus.engines = with pkgs.ibus-engines; [ rime ];
   };
 }
