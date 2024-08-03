600b4197f8799fd95ad84acca9c28f802a4de33b
ChaosAttractor <lostattractor@gmail.com>
2024-08-04 01:23:23 +0800

    specific/general/nvidia/cuda: init

--- specific/hardware-specific/asus-zephyrus-ga401/default.nix
+++ specific/hardware-specific/asus-zephyrus-ga401/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ../../general/amd/virtualisation.nix
+    ../../general/nvidia/cuda.nix
     ../../general/nvidia/open.nix
     ../../general/nvidia/beta.nix
     # ../../general/nvidia/specialisation.nix
