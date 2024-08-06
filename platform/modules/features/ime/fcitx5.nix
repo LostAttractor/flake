e7a398d8ebb35dfed9c0fcc0c79b43df49611d56
ChaosAttractor <lostattractor@gmail.com>
2024-08-07 06:54:11 +0800

    platform/desktop/modules: reclassification

--- platform/modules/features/ime/fcitx5.nix
+++ platform/modules/features/ime/fcitx5.nix
@@ -3,6 +3,9 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons ];
+    fcitx5.addons = with pkgs; [
+      fcitx5-rime
+      fcitx5-chinese-addons
+    ];
   };
 }
