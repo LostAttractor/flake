ca0becc1f81984a7179a501af50938a81e111a25
ChaosAttractor <lostattractor@gmail.com>
2024-12-07 03:41:12 +0800

    packages/gaming: anime-brob-launcher is deprecated

--- packages/gaming.nix
+++ packages/gaming.nix
@@ -27,7 +27,7 @@
 
   nix.settings = inputs.aagl.nixConfig; # Set up Cachix
   programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
-  programs.anime-borb-launcher.enable = true;
+  programs.anime-games-launcher.enable = true;
   programs.honkers-railway-launcher.enable = true;
   programs.honkers-launcher.enable = true;
   programs.wavey-launcher.enable = true;
