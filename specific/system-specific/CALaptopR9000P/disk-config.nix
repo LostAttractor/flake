2ed5888aee7925daa7f480ba0724eb64701b9138
ChaosAttractor <lostattractor@gmail.com>
2025-01-09 03:48:02 +0800

    flake: CALaptopR900P reinit

---
+++ specific/system-specific/CALaptopR9000P/disk-config.nix
@@ -0,0 +1,76 @@
+_:
+{
+    fileSystems."/" = {
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = [ "mode=755" ];
+  };
+
+  fileSystems."/persistent".neededForBoot = true;
+
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "/dev/nvme0n1";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          size = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
+          };
+        };
+        luks = {
+          end = "-32G";
+          content = {
+            type = "luks";
+            name = "crypted";
+            passwordFile = "/tmp/secret.key"; # Interactive
+            settings.allowDiscards = true;
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/persistent" = {
+                  mountpoint = "/persistent";
+                  mountOptions = [ "compress=zstd" "discard=async" ];
+                };
+                "/root" = {
+                  mountpoint = "/root";
+                  mountOptions = [ "compress=zstd" "discard=async" ];
+                };
+                "/home" = {
+                  mountpoint = "/home";
+                  mountOptions = [ "compress=zstd" "discard=async" ];
+                };
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [ "compress=zstd" "discard=async" "noatime" ];
+                };
+              };
+            };
+          };
+        };
+        encryptedSwap = {
+          size = "100%";
+          content = {
+            type = "swap";
+            randomEncryption = true;
+            discardPolicy = "both";
+          };
+        };
+      };
+    };
+  };
+
+  services.beesd.filesystems.root = {
+    spec = "/dev/mapper/crypted";
+    hashTableSizeMB = 128; # 128K extent size for 1TB
+    verbosity = "crit";
+    extraOptions = [ "--loadavg-target" "5.0" ];
+  };
+}
